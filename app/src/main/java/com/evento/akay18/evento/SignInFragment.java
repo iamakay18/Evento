fe7726b135a162a329f800852ad306329255410d
akshaykumar <iamakay18@gmail.com>
2017-12-27 14:40:55 +0530

    27-12-17

--- app/src/main/java/com/evento/akay18/evento/SignInFragment.java
+++ app/src/main/java/com/evento/akay18/evento/SignInFragment.java
@@ -79,8 +79,6 @@ public class SignInFragment extends Fragment {
         //Instantiate Broadcast receiver.
         mReceiver = new NetworkReceiver();
 
-
-
         //Intent Filter will receive System Broadcast.
         IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
         //Register Broadcast receiver with the filter.
